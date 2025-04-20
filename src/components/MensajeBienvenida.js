5fd18bd25e6e5084a7b4822940922492ec7a3ec5
andrea-otero <422032407@pcpuma.acatlan.unam.mx>
2025-04-19 20:00:16 -0600

    Create MensajeBienvenida.js

---
+++ src/components/MensajeBienvenida.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MensajeBienvenida() {
+  return (
+    <div>
+      <h2>Este es un componente nuevo</h2>
+      <p>¡React es genial!</p>
+    </div>
+  );
+}
+
+export default MensajeBienvenida;
